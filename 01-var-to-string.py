3b5afd80cd184cffacde8e24dd62bfc44c013efb
gzelle007 <79612991+gzelle007@users.noreply.github.com>
2021-10-13 08:26:19 +1300

    Add files via upload

---
+++ 01-var-to-string.py
@@ -0,0 +1,13 @@
+# Setting variable to integer
+my_score = 201
+
+# variable inserted into string
+print("The type of my_score is ", type(my_score),"\n")
+print("my_score is ", my_score,"\n")
+ 
+print("Casting to a string...\n")
+# variable cast to string
+my_score = str(my_score)
+ 
+print("The type of my_score is now ", type(my_score),"\n")
+print("my_score is ", my_score,"\n")
